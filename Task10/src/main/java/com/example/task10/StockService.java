49b482a6e57b8ff1612dec5a2072bb386ae6a249
sofijasljusar <sofija.sljusar@gmail.com>
2025-04-11 00:18:19 +0300

    Create singleton class as option to handle screen navigation and data consistency

---
+++ Task10/src/main/java/com/example/task10/StockService.java
@@ -0,0 +1,10 @@
+//package com.example.task10;
+//
+//public class StockService {
+//    private static final StockExchange instance = new StockExchange(2);
+//
+//    public static StockExchange getInstance() {
+//        return instance;
+//    }
+//}
+//
